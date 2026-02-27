6411b2ac1eef239961d4c4c4843de771c467619c
ARTHUR BORGES CORREIA <232.999257@universidadesenaicimatec.edu.br>
2026-02-26 21:35:15 -0300

    banco de dados

--- src/main/java/login/Login.java
+++ src/main/java/login/Login.java
@@ -1,34 +1,53 @@
 package login;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-
+import connectio.ConnectionFactory;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
+
+/**
+ *
+ * @author EJADEN0058
+ */
 @WebServlet("/login")
-public class Login extends HttpServlet {
-
+public class Login extends HttpServlet{
+    
     private static final long serialVersionUID = 1L;
-
+    
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-
+        
         String usuario = request.getParameter("users");
         String senha = request.getParameter("passw");
+        
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
         
-        if("admin".equals(usuario)){
-        response.sendRedirect("Pagina1.html");
-        //out.println("<h2>login realizado</h2>");
+        try (var con = ConnectionFactory.getConnection()){
+            String sql = "SELECT * FROM users WHERE username= ? AND psw= ?";
             
-        }else{
-            out.println("<h2>Usuario ou senha incorretos</h2>");
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setString(1, usuario);
+            stmt.setString(2, senha);
+            
+            ResultSet rs = stmt.executeQuery();
+            
+            if(rs.next()){
+                response.sendRedirect("dashboard.html");
+            }else {
+                out.println("<h2>Dados errados.</h2>");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            out.println("<h2>Erro ao conectar ao banco de dados.</h2>");
         }
-
-}
+  
+    }
 }
